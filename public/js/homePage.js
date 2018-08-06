25afbdb388aea82e5b7ef7363b4e5996f6355048
cli <cli@mastercontrol.com>
2018-08-05 19:18:23 -0600

    got buttons to return specific ids with matching difficulty

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -1,9 +1,63 @@
 $(document).ready(function () {
-    // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
-    $.get("/api/user_data").then(function (data) {
-        $(".member-name").text(data.email);
+
+    var easyBtnRandomizer = $(".easybtn");
+    var mediumBtnRandomizer = $(".mediumbtn");
+    var hardBtnRandomizer = $(".hardbtn");
+
+    easyBtnRandomizer.on("click", function (event) {
+        event.preventDefault();
+        var difficulty = "easy";
+
+        getAdventureList(difficulty);
+
     });
 
-    
+    mediumBtnRandomizer.on("click", function (event) {
+        event.preventDefault();
+        var difficulty = "medium";
+
+        getAdventureList(difficulty);
+    });
+
+    hardBtnRandomizer.on("click", function (event) {
+        event.preventDefault();
+        var difficulty = "hard";
+
+        getAdventureList(difficulty);
+    });
+
+    function getAdventureList(difficulty) {
+        if (difficulty === "easy") {
+            $.get("/api/adventures", function (data) {
+                var listofIds = [];
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].difficulty === "easy") {
+                        listofIds.push(data[i].id);
+                    }
+                }
+                console.log(listofIds);
+            });
+        } else if (difficulty === "medium") {
+            $.get("/api/adventures", function (data) {
+                var listofIds = [];
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].difficulty === "medium") {
+                        listofIds.push(data[i].id);
+                    }
+                }
+                console.log(listofIds);
+            });
+        } else if (difficulty === "hard") {
+            $.get("/api/adventures", function (data) {
+                var listofIds = [];
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].difficulty === "hard") {
+                        listofIds.push(data[i].id);
+                    }
+                }
+                console.log(listofIds);
+            });
+        }
+    }
+
 });
