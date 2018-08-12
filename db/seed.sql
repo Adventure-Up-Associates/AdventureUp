850481e172527063a5d76a99bdb2699381466662
cli <cli@mastercontrol.com>
2018-08-12 16:01:44 -0600

    added to the seed.sql file

--- db/seed.sql
+++ db/seed.sql
@@ -19,3 +19,38 @@ VALUES ("Climb Mount Olympus!",
 "Mount Olympus, Utah", "hard"
 );
 
+INSERT INTO Adventures (title, description, location, difficulty)
+VALUES ("Keys on Main",
+"If you're a fan of top-notched improvised piano performances, and loudly karaoking with a bunch of semi-drunk people, then check out Keys on Main in the evenings around 9. Two people on opposing pianos will take song requests and begin playing them while interacting with the crowd. If you have a group of friends that enjoy that type of activity and will go with you there, it's guaranteed to be a blast.",
+"242 Main St, Salt Lake City, Utah", "easy"
+);
+
+INSERT INTO Adventures (title, description, location, difficulty)
+VALUES ("Walking the Little Cottonwood Trail",
+"If you want to go on a hike, want to go through the woods, but don't want to be walking up slopes or maybe it's just too hot outside, this is a perfect alternative. The trail runs parallel to the Little Cottonwood Creek most of the way, and is constantly in the shades of trees. It's a mostly flat trek, and a respectable 5.1 miles from beginning to end. If you like biking, it also does allow bikes.",
+"Little Cottonwood Trail, Granite, Utah", "medium"
+);
+
+INSERT INTO Adventures (title, description, location, difficulty)
+VALUES ("Visit the Utah Olympic Park in Park City",
+"To call it a conventional park may be a little bit of a stretch, but it was a sports park built for the 2002 Winter Olympics. There a number of fun activities of varying difficulty levels that you can do once there. You can zipline from the top of the park all the way down to the bottom, try your hands at obstacle courses, or bungie jumping. Fun for people that like slight craziness, but not overly crazy activies.",
+"3419 Olympic Pkwy, Park City, UT 84098", "medium"
+);
+
+INSERT INTO Adventures (title, description, location, difficulty)
+VALUES ("Bushwhack your way through the Emigration Canyon Ridgeline Trail", 
+"If you haven't gone bushwhacking before on a trail, it's definitely an exciting experience.Trailblazing through unmarked paths provides an exhilarating rush, and when you finally get on top of the ridgeline the view is gorgeous. Easier with a someone experienced, but if you want the challenge, it's doable without one. Just make sure to bring ample food and water, and leave early, as it is a fairly long trail, at a little over 8 miles.",
+"#80 of 352 trails in Uinta-Wasatch-Cache National Forest, Utah", "hard"
+);
+
+INSERT INTO Adventures (title, description, location, difficulty)
+VALUES ("Reading a book series backwards",
+"I once read Orson Scott Card's Ender's Game series mostly backwards, jumping to the last entry in the series after finishing the namesake book of the series. It was an interesting way of consuming the story, as existing character personalities and motivations supposedly clear would be unclear. As a result, the meaning behind events are delayed, often only hinted at, and obscured until an earlier book is read where the character's backstory is explored. If you haven't done this at least once, give it a try.",
+"In the comforts of your own home", "medium"
+);
+
+INSERT INTO Adventures (title, description, location, difficulty)
+VALUES ("Challenge yourself to self reflection",
+"These days our lives are constantly moving and changing in every possible way. The person that we were a year ago is not the same person that exists today. Yet we keep moving forward under the same goals and motivations set by our earlier selves, because we don't know any better. This is stumbling through life. Meditate on your new self, constantly update yourself on what your life goals and most important aspects are. Reset your direction.",
+"Somewhere where you will not be disturbed", "medium"
+);
